053e6f2d902e55e816b2ffffd8b20cdd37efbd1a
baiv84 <65267292+baiv84@users.noreply.github.com>
2023-12-21 22:53:13 +0700

    Fix .gitignore

--- src/main/java/tools/LongRandomNumberGenerator.java
+++ src/main/java/tools/LongRandomNumberGenerator.java
@@ -7,20 +7,10 @@ public final class LongRandomNumberGenerator {
 
     private PrimitiveIterator.OfLong randomIterator;
 
-    /**
-     * Initialize a new random number generator that generates
-     * random numbers in the range [min, max]
-     * @param min - the min value (inclusive)
-     * @param max - the max value (inclusive)
-     */
     public LongRandomNumberGenerator(long min, long max) {
         randomIterator = new Random().longs(min, max + 1).iterator();
     }
 
-    /**
-     * Returns a random number in the range (min, max)
-     * @return a random number in the range (min, max)
-     */
     public long nextLong() {
         return randomIterator.nextLong();
     }
